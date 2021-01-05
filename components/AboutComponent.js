1865ed8e5163a52545065ccae8dab1b1972ea86f
Chris <chawkins2010@hotmail.com>
2021-01-05 14:41:56 -0600

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import { Card} from 'react-native-elements';
+import { Card, ListItem} from 'react-native-elements';
 import { Text } from 'react-native';
-import { ListItem } from 'react-native-elements';
 import { FlatList, ScrollView } from 'react-native-gesture-handler';
-import { PARTNERS} from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
+
 
 function Misson(){
     return(
@@ -15,14 +22,7 @@ function Misson(){
     );
 }
 
-class About extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-           partners: PARTNERS
-        };
-    }
+class About extends Component {    
 
     static navigationOptions = {
         title: 'About Us'
@@ -34,7 +34,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
                 />
            );
        };
@@ -46,7 +46,7 @@ class About extends Component {
                     title={"Community Partners"}
                 >
                     <FlatList 
-                        data={this.state.partners}
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
                     />
@@ -55,4 +55,4 @@ class About extends Component {
         );
     }
 }
-export default About;
+export default connect(mapStateToProps)(About);
